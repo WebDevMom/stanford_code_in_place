75fcbcd0064662a4429c1fe69215d7aa1d6cc34a
Carmen Smith <cheylaracarm77@gmail.com>
2023-05-01 20:22:41 -0500

    mountain_climb.py

---
+++ mountain_climb.py
@@ -0,0 +1,33 @@
+from stanfordkarel import *
+
+
+def main():
+    climb_up()
+    put_beeper()
+    climb_down()
+
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+def climb_up():
+    while front_is_blocked():
+        turn_left()
+        move()
+        turn_right()
+        move()
+
+
+def climb_down():
+    while front_is_clear():
+        move()
+        turn_right()
+        move()
+        turn_left()
+
+
+if __name__ == '__main__':
+    run_karel_program()
